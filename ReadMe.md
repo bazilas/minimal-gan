37d760afac5d0dc8ff4b766d06389bca24916dfa
Vasilis Belagiannis <belajohn@gmail.com>
2017-09-30 22:43:19 +0200

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,34 @@
+# Minimal GAN in TensorFlow - 101 lines
+This is an implementation of a Generative Adversarial Network (GAN) in [TensorFlow](http://tensorflow.org). The example is based on the original work from [Goodfellow et. al. ](https://arxiv.org/abs/1406.2661), where the generator learns to reconstruct samples from the [MNIST](http://yann.lecun.com/exdb/mnist/) dataset.
+
+## Dependencies
+The code has been tested in [TensorFlow](http://tensorflow.org) 1.3 (Python 3.6, CPU, macOS). It should work in other platforms as well. Also, it makes use of  [TensorFlow-Slim](https://github.com/tensorflow/tensorflow/tree/master/tensorflow/contrib/slim).
+
+## Execution
+
+Run the following script:
+
+    $ python gan.py
+
+The results after ~200k iterations should look like the following:
+
+![result](figures/0178000.png)
+
+## Acknowledgement
+Two helpful repositories with several GAN implmentations are [wiseodd](https://github.com/wiseodd/generative-models) and [hwalsuklee](https://github.com/hwalsuklee/tensorflow-generative-model-collections). This implementation drew inspiration from the aformentioned repositories.
+
+## License
+Copyright (c) 2017,  V. Belagiannis
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ gan.py
@@ -0,0 +1,101 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+from tensorflow.examples.tutorials.mnist import input_data
+
+# Noise dimensions
+zdim = 50
+
+# Samples
+Nsamples = 100
+
+# Input to the discriminator
+x = tf.placeholder(tf.float32, shape=[None, 784])
+
+# Input to the generator
+z = tf.placeholder(tf.float32, shape=[None, zdim])
+
+# Load MNIST data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+# Define the generator
+def generator(input, is_training=True, reuse=False):
+    with tf.variable_scope("generator", reuse=reuse):
+        net = slim.fully_connected(input, 128, activation_fn=tf.nn.relu, trainable=is_training, reuse=reuse, scope='gen1')
+        net = slim.fully_connected(net, 784, activation_fn=tf.sigmoid, trainable=is_training, reuse=reuse, scope='gen2')
+        return net
+
+# Define the discriminator
+def discriminator(input, is_training=True, reuse=False):
+    with tf.variable_scope("discriminator", reuse=reuse):
+        net = slim.fully_connected(input, 128, activation_fn=tf.nn.relu, trainable=is_training, reuse=reuse, scope='dic1')
+        logit = slim.fully_connected(net, 1, activation_fn=None, trainable=is_training, reuse=reuse, scope='dic2')
+        prob = tf.nn.sigmoid(logit)
+        return logit, prob
+
+# Generate samples
+def draw_sample(m, n):
+    return np.random.uniform(-1.0, 1.0, size=[m, n])
+
+# Inference: Generate Sample, Classify real / generated sample
+G_sample_train = generator(z)
+D_logit_real, D_real = discriminator(x)
+D_logit_gen, D_fake = discriminator(G_sample_train, reuse=True)
+G_sample_inf = generator(z, is_training=False, reuse=True)
+
+# Loss Functions
+D_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_real, labels=tf.ones_like(D_logit_real))) + \
+         tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_gen, labels=tf.zeros_like(D_logit_gen)))
+G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_gen, labels=tf.ones_like(D_logit_gen)))
+
+# All variables
+t_vars = tf.trainable_variables()
+
+# Generator variables (used during G update)
+g_vars = [var for var in t_vars if 'generator' in var.name]
+
+# Discriminator variables (used during D update)
+d_vars = [var for var in t_vars if 'discriminator' in var.name]
+
+# Optimizers
+D_opt = tf.train.AdamOptimizer().minimize(loss=D_loss, var_list=d_vars)
+G_opt = tf.train.AdamOptimizer().minimize(loss=G_loss, var_list=g_vars)
+
+with tf.Session() as sess:
+
+    # Initialize all variables
+    tf.global_variables_initializer().run()
+
+    # training iterations
+    for i in range(int(1e6)):
+        imgs, labels = mnist.train.next_batch(Nsamples)
+
+        # 1. Update the discriminator using real and generated samples
+        feed_dict_d = {x: imgs, z: draw_sample(Nsamples, zdim)}
+        _, D_loss_val = sess.run([D_opt, D_loss], feed_dict=feed_dict_d)
+
+        # 2. Update the generator
+        feed_dict_g = {z: draw_sample(Nsamples, zdim)}
+        _, G_loss_val = sess.run([G_opt, G_loss], feed_dict=feed_dict_g)
+
+        if i % int(1e3) == 0:
+            print('Iteration {}, Discriminator Loss {:.3}, Generator Loss {:.3}'.format(i, D_loss_val, G_loss_val))
+
+            # sample from the generator
+            sample = sess.run(G_sample_inf, feed_dict=feed_dict_g)
+
+            # Visualise the sampled images
+            fig = plt.figure(figsize=(10, 10))
+            gs = gridspec.GridSpec(10, 10)
+            for j, sample in enumerate(sample):
+                ax = plt.subplot(gs[j])
+                ax.set_xticks([])
+                ax.set_yticks([])
+                plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
+            if not os.path.exists('res'):
+                os.makedirs('res')
+            plt.savefig('res/{}.png'.format(str(i).zfill(7)), bbox_inches='tight')
+            plt.close()
